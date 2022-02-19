7cd0f07743fba1fea18112c4c3322670389648a8
Calin Timofte <calin.timofte@yahoo.com>
2022-02-19 07:22:14 +0200

    deploy setup

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     })
 
 let corsOptions = {
-    origin: ["http://localhost:3001"]
+    origin: ["http://localhost:" + process.env.PORT || 3001]
 };
 
 app.use(cors(corsOptions));
